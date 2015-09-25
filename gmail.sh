c7f1c3ac16e280792747afba840dc8cb51f019d8
Gregorio Martinez <greg@gmartinez.com>
2015-09-25 11:38:55 -0400

    Add icon for gmail script

--- gmail.sh
+++ gmail.sh
@@ -8,7 +8,7 @@ source /home/greg/scripts/i3block-scripts/gmail.conf
 
 MAILCOUNTER=`curl -u $USERID:$PASSWORD --silent "https://mail.google.com/mail/feed/atom" | xmllint --format --xpath "string(//*[name() = 'feed']/*[name() = 'fullcount'])" -`
 if [[ "$MAILCOUNTER" = "" ]]; then
-    echo "ERROR: The program couldn't fetch the account for \"$USERID\"."
+    echo "ERROR"
 else
-    echo "$MAILCOUNTER new emails"
+    echo "&#xf0e0;  $MAILCOUNTER"
 fi
