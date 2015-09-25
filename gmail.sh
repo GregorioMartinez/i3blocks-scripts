f006e173b62d8d75fcccbd3335e482289626c5df
Gregorio Martinez <greg@gmartinez.com>
2015-09-24 21:18:13 -0400

    Adding conf file for gmail script

--- gmail.sh
+++ gmail.sh
@@ -4,6 +4,8 @@
 # curl
 # xmllint
 
+source gmail.conf
+
 MAILCOUNTER=`curl -u $USERID:$PASSWORD --silent "https://mail.google.com/mail/feed/atom" | xmllint --format --xpath "string(//*[name() = 'feed']/*[name() = 'fullcount'])" -`
 if [[ "$MAILCOUNTER" = "" ]]; then
     echo "ERROR: The program couldn't fetch the account for \"$USERID\"."
