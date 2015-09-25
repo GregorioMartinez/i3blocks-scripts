a13f55d8e31f68470be51395f50d5f23f4b67274
Gregorio Martinez <greg@gmartinez.com>
2015-09-24 21:17:11 -0400

    Gmail script

---
+++ gmail.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Dependencies
+# curl
+# xmllint
+
+MAILCOUNTER=`curl -u $USERID:$PASSWORD --silent "https://mail.google.com/mail/feed/atom" | xmllint --format --xpath "string(//*[name() = 'feed']/*[name() = 'fullcount'])" -`
+if [[ "$MAILCOUNTER" = "" ]]; then
+    echo "ERROR: The program couldn't fetch the account for \"$USERID\"."
+else
+    echo "$MAILCOUNTER new emails"
+fi
