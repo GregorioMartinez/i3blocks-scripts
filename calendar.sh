e549ebfa36816f1af06de4ef27dccd688d7d4b82
Gregorio Martinez <greg@gmartinez.com>
2015-09-28 17:34:17 -0400

    Set calendar to open on click

--- calendar.sh
+++ calendar.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+case $BLOCK_BUTTON in
+	1)
+		google-chrome http://calendar.google.com > /dev/null 2>&1
+esac
+
 DATE=`date '+%D %I:%M'`
 
-echo "&#xf073; $DAT"
+echo "&#xf073; $DATE"
