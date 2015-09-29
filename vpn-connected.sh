93279f716ef297ff6c391d540466cd3a50081a41
Gregorio Martinez <greg@gmartinez.com>
2015-09-29 08:36:18 -0400

    Removing unused comment

--- vpn-connected.sh
+++ vpn-connected.sh
@@ -8,7 +8,6 @@
 IP=`dig +short myip.opendns.com @resolver1.opendns.com`
 
 COLOR="F2777A"
-# VPN=`test -e  && echo $IP || echo "VPN Not Connected"`
 
 if test -e /proc/sys/net/ipv4/conf/tun0; then
 	COLOR="99CC99"
