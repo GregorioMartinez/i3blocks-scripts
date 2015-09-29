c70ed41fad3607d3b59cc586f2dffa1dc1a7e0b4
Gregorio Martinez <greg@gmartinez.com>
2015-09-28 22:50:18 -0400

    Add colors to battery script

--- battery.sh
+++ battery.sh
@@ -6,17 +6,22 @@
 # tr
 
 BAT=`acpi -b | grep -Eo [0-9]+\% | tr -d '%'`
-
 ICON="f244"
+COLOR="F2777A"
+
 if [[ $BAT -gt 90 ]]; then
 	ICON="f240"
+	COLOR="99CC99"
 elif [[ $BAT -gt 75 ]]; then
 	ICON="f241"
+	COLOR="99CC99"
 elif [[ $BAT -gt 50 ]]; then
 	ICON="f242"
+	COLOR="F99157"
 elif [[ $BAT -gt 25 ]]; then
 	ICON="f243"
+	COLOR="F2777A"
 fi
 
-echo "<span font_desc='FontAwesome'>&#x$ICON; $BAT%</span>" 
+echo "<span color=\"#$COLOR\" font_desc='FontAwesome'>&#x$ICON; $BAT%</span>" 
 # echo $BAT
