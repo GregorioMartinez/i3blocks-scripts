1812bea66913b83bc2e947568dcdd3a7c13a90ad
Gregorio Martinez <greg@gmartinez.com>
2015-09-28 20:49:41 -0400

    Add battery icons to battery script

--- battery.sh
+++ battery.sh
@@ -3,7 +3,20 @@
 # Dependencies
 # acpi
 # grep
+# tr
 
-BAT=`acpi -b | grep -Eo [0-9]+\%`
+BAT=`acpi -b | grep -Eo [0-9]+\% | tr -d '%'`
 
-echo $BAT
+ICON="f244"
+if [[ $BAT -gt 90 ]]; then
+	ICON="f240"
+elif [[ $BAT -gt 75 ]]; then
+	ICON="f241"
+elif [[ $BAT -gt 50 ]]; then
+	ICON="f242"
+elif [[ $BAT -gt 25 ]]; then
+	ICON="f243"
+fi
+
+echo "<span font_desc='FontAwesome'>&#x$ICON; $BAT%</span>" 
+# echo $BAT
