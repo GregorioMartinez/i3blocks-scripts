730e40857558ad255366107df9ecf7a15069feb1
Gregorio Martinez <greg@gmartinez.com>
2015-11-23 14:09:44 -0500

    Add battery plug for charging

--- battery.sh
+++ battery.sh
@@ -5,23 +5,29 @@
 # grep
 # tr
 
-BAT=`acpi -b | grep -Eo [0-9]+\% | tr -d '%'`
+BAT=`acpi -b`
+LEVEL=`echo $BAT | grep -Eo [0-9]+\% | tr -d '%'`
+ISCHARGING=`echo $BAT | grep -Eo Charging`
+
 ICON="f244"
 COLOR="F2777A"
 
-if [[ $BAT -gt 90 ]]; then
+if [[ $LEVEL -gt 90 ]]; then
 	ICON="f240"
 	COLOR="99CC99"
-elif [[ $BAT -gt 75 ]]; then
+elif [[ $LEVEL -gt 75 ]]; then
 	ICON="f241"
 	COLOR="99CC99"
-elif [[ $BAT -gt 50 ]]; then
+elif [[ $LEVEL -gt 50 ]]; then
 	ICON="f242"
 	COLOR="F99157"
-elif [[ $BAT -gt 25 ]]; then
+elif [[ $LEVEL -gt 25 ]]; then
 	ICON="f243"
 	COLOR="F2777A"
 fi
 
-echo "<span color=\"#$COLOR\" font_desc='FontAwesome'>&#x$ICON; $BAT%</span>" 
-# echo $BAT
+if [ "$ISCHARGING" == "Charging" ]; then
+    ICON="f1e6"
+fi
+
+echo "<span color=\"#$COLOR\" font_desc='FontAwesome'>&#x$ICON; $LEVEL%</span>" 
